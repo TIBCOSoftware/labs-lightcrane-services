568f4bd124529e1953e7b5096ec405005d78bc37
Miguel Torres <migueltorreslopez@gmail.com>
2022-04-22 08:46:17 -0700

    Add error exit code

--- docker-compose/oss/export.sh
+++ docker-compose/oss/export.sh
@@ -3,18 +3,18 @@ mkdir -p ./archives
 
 source .env
 
-docker-compose pull
+docker-compose pull || exit 1
 
-docker save --output ./archives/labs-lightcrane-proxy-${LABS_AIR_VERSION}.tar public.ecr.aws/tibcolabs/labs-lightcrane-proxy:${LABS_AIR_VERSION} || exit 2
+docker save --output ./archives/labs-lightcrane-proxy-${LABS_AIR_VERSION}.tar public.ecr.aws/tibcolabs/labs-lightcrane-proxy:${LABS_AIR_VERSION} || exit 1
 
-docker save --output ./archives/labs-lightcrane-servicelocator-${LABS_AIR_VERSION}.tar public.ecr.aws/tibcolabs/labs-lightcrane-servicelocator:${LABS_AIR_VERSION}
+docker save --output ./archives/labs-lightcrane-servicelocator-${LABS_AIR_VERSION}.tar public.ecr.aws/tibcolabs/labs-lightcrane-servicelocator:${LABS_AIR_VERSION} || exit 1
 
-docker save --output ./archives/labs-lightcrane-projectmgr-${LABS_AIR_VERSION}.tar public.ecr.aws/tibcolabs/labs-lightcrane-projectmgr:${LABS_AIR_VERSION}
+docker save --output ./archives/labs-lightcrane-projectmgr-${LABS_AIR_VERSION}.tar public.ecr.aws/tibcolabs/labs-lightcrane-projectmgr:${LABS_AIR_VERSION} || exit 1
 
-docker save --output ./archives/labs-lightcrane-builder-${LABS_AIR_VERSION}.tar public.ecr.aws/tibcolabs/labs-lightcrane-builder:${LABS_AIR_VERSION}
+docker save --output ./archives/labs-lightcrane-builder-${LABS_AIR_VERSION}.tar public.ecr.aws/tibcolabs/labs-lightcrane-builder:${LABS_AIR_VERSION} || exit 1
 
-docker save --output ./archives/labs-lightcrane-deployer-${LABS_AIR_VERSION}.tar public.ecr.aws/tibcolabs/public.ecr.aws/tibcolabs/labs-lightcrane-deployer:${LABS_AIR_VERSION}
+docker save --output ./archives/labs-lightcrane-deployer-${LABS_AIR_VERSION}.tar public.ecr.aws/tibcolabs/public.ecr.aws/tibcolabs/labs-lightcrane-deployer:${LABS_AIR_VERSION} || exit 1
 
-docker save --output ./archives/labs-lightcrane-air-service-${LABS_AIR_VERSION}.tar public.ecr.aws/tibcolabs/labs-lightcrane-air-service:${LABS_AIR_VERSION}
+docker save --output ./archives/labs-lightcrane-air-service-${LABS_AIR_VERSION}.tar public.ecr.aws/tibcolabs/labs-lightcrane-air-service:${LABS_AIR_VERSION} || exit 1
 
-docker save --output ./archives/labs-lightcrane-model-service-${LABS_AIR_VERSION}.tar public.ecr.aws/tibcolabs/labs-lightcrane-model-service:${LABS_AIR_VERSION}
+docker save --output ./archives/labs-lightcrane-model-service-${LABS_AIR_VERSION}.tar public.ecr.aws/tibcolabs/labs-lightcrane-model-service:${LABS_AIR_VERSION} || exit 1
