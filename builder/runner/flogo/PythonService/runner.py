b9bcdb36bc6eb05c3c28d0b6fcbed3aaedc3ec0d
Infospecktra <Infospecktra@gmail.com>
2022-04-26 15:15:28 -0400

    New Feature
    Python support for air pipeline

--- builder/runner/flogo/PythonService/runner.py
+++ builder/runner/flogo/PythonService/runner.py
@@ -2,7 +2,6 @@
 
 from __future__ import print_function
 from concurrent import futures
-import logging
 
 import grpc
 
@@ -10,27 +9,45 @@ import pipecoupler_pb2
 import pipecoupler_pb2_grpc
 
 import os
+import os.path
 import importlib
 import inspect
 import json
+import logging
+
+#from f1 import serviceLocator as locator
+from logging.handlers import RotatingFileHandler
 
 # environment variables 
 env_var = os.environ
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+#        logging.FileHandler("/home/log/air_pipeline.log"),
+        RotatingFileHandler("/home/log/air_pipeline.log", maxBytes=250000, backupCount=5),
+        logging.StreamHandler()
+    ]
+)
 
 class ProcessorService():
     def __init__(self):
         print('(ProcessorService.__init__) entering ...... ')
         self.processors = {}
-        print('(ProcessorService.__init__) request=%s' % (os.listdir('.')))
-        for filename in os.listdir('.'):
-            if filename.endswith('.py') :
+        print('(ProcessorService.__init__) files ={}'.format(os.listdir('.')))
+        
+        for key in env_var:
+            if key.endswith('Python_Processor'):
+                filename = env_var[key]
+                print('(ProcessorService.__init__) register : {}'.format(filename))
                 processorName = filename[0:len(filename)-3]
                 print('(ProcessorService.__init__) processorName : {}'.format(processorName))
-                module = importlib.import_module('artifacts.{}'.format(processorName))
-                print(inspect.isfunction(module.Processor.evaluate))
-                if inspect.isfunction(module.Processor.evaluate) :
-                    processor = module.Processor(os.environ)
-                    self.processors['{}.py'.format(processorName)] = processor
+                module = importlib.import_module(processorName)
+                processor = module.Processor(os.environ)
+                self.processors['{}.py'.format(processorName)] = processor
+
+        print('(ProcessorService.__init__) self.processors = {}'.format(self.processors))
         print('(ProcessorService.__init__) done ...... ')
 
     def HandleData(self, request, context):
@@ -44,7 +61,6 @@ class ProcessorService():
         return pipecoupler_pb2.Reply(sender='ProcessorService', ID=ID, content=json.dumps(result), status=True)
 
 def serve():
-    logging.basicConfig()
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     pipecoupler_pb2_grpc.add_PipeCouplerServicer_to_server(ProcessorService(), server)
     grpcport = '9998'
--- builder/runner/flogo/PythonService/server.py
+++ builder/runner/flogo/PythonService/server.py
@@ -9,19 +9,17 @@ print (sys.version_info)
 print("Path info")
 print (sys.path)
 
-#['/app', '/usr/lib/python37.zip', '/usr/lib/python3.7', '/usr/lib/python3.7/lib-dynload', '/home/atlas/.local/lib/python3.7/site-packages', '/usr/lib/python3.7/site-packages']
-
 env_var = os.environ
 # Print the list of user's 
 # environment variables 
-print("User's Environment variable:") 
-pprint.pprint(dict(env_var), width = 1) 
+#print("User's Environment variable:") 
+#pprint.pprint(dict(env_var), width = 1) 
 
 # Setup working folder
-working_folder = '{}/artifacts'.format(os.getcwd())
-if "Working_Folder" not in env_var:
-    env_var['Working_Folder'] = working_folder
-os.chdir(working_folder)
+#working_folder = '{}/artifacts'.format(os.getcwd())
+#if "Working_Folder" not in env_var:
+#    env_var['Working_Folder'] = working_folder
+#os.chdir(working_folder)
 
 if __name__ == '__main__':
     if 'System_Standalone' in env_var and 'True' == env_var['System_Standalone'] :
