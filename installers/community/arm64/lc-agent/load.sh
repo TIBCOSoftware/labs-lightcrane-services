68f0cda508fd4ce314f02f7fdd754dc75c1ec00c
Miguel Torres <migueltorreslopez@gmail.com>
2022-05-04 18:30:49 -0700

    Add specific arm64 images

--- installers/community/arm64/lc-agent/load.sh
+++ installers/community/arm64/lc-agent/load.sh
@@ -7,4 +7,4 @@ docker load --input "./archives/labs-lightcrane-agent-arm64:${LABS_AIR_VERSION}.
 
 docker load --input "./archives/labs-lightcrane-pyservice-base-arm64:${LABS_AIR_VERSION}.tar" || exit 1
 
-docker load --input "./archives/arm64v8/ubuntu:${UBUNTU_VERSION}.tar" || exit 1
+docker load --input "./archives/arm64v8_ubuntu:${UBUNTU_VERSION}.tar" || exit 1
