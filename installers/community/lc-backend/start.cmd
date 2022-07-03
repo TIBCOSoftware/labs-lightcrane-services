a7a13a200a404dfd44bb91180af333eaf9faa7f7
magallardo <gallardo@tibco.com>
2022-07-03 19:37:57 -0400

    Updated windows scripts.

--- installers/community/lc-backend/start.cmd
+++ installers/community/lc-backend/start.cmd
@@ -8,5 +8,10 @@ set InternetAccess=false
 set ServiceLocatorExternal=localhost:5408
 set ExternalEndpointIP=localhost
 if not exist "%LC_HOME%" mkdir "%LC_HOME%"
+
+if "%1%"==""  set ARCH=""
+if "%1%"=="amd64"  set ARCH=""
+if "%1%"=="arm64"  set ARCH="-arm64"
+
 docker-compose rm -f
 docker-compose up -d
